c28b8c978cc5773380ea9ce5b5a04e35c266af54
vincentmsx <mousseaux.v@gmail.com>
2026-06-28 13:46:15 +0200

    s

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,21 @@
+// 1. Declare the type on the global window object
+declare global {
+  interface Window {
+    env?: {
+      envName: string;
+    };
+  }
+}
+
+// 2. Log to console (you see this in F12)
 console.log("Hello world !");
+
+// 3. Add to the page (you see this in the browser window)
+const currentEnv = window.env?.envName || "LOCAL";
+
+const app = document.querySelector('body');
+if (app) {
+  const newElement = document.createElement("p");
+  newElement.textContent = `Hello World! Current environment: ${currentEnv}`;
+  app.appendChild(newElement);
+}
